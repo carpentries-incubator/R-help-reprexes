dcaaa6f84359e198cf91413dfaaa81b7f3877710
peterlaurin <69608241+peterlaurin@users.noreply.github.com>
2024-07-23 13:53:46 -0700

    Add ratdat toy example with lubridate
    using lubridate and year vs. time format, looking at observations for each enclosure in kangaroo rats

---
+++ ratdat_lubridate_toy_example.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(lubridate)
+library(ratdat)
+rat_data <- ratdat::complete
+rat_data <- rat_data %>% mutate(time = mdy(paste(month, day, year)))
+
+dips <- rat_data %>% filter(genus == "Dipodomys")
+ggplot(dips) + geom_point(aes(time, weight, color=plot_type)) + facet_wrap(~species)
+
+# time plots
+ggplot(dips, aes(time, fill = plot_type)) + geom_histogram()  + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+#month
+ggplot(dips, aes(time, fill = plot_type)) + geom_histogram(binwidth = 30)  + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+#year
+ggplot(dips, aes(time, fill = plot_type)) + geom_histogram(binwidth = 365)  + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+
+
+# year plots
+#year
+ggplot(dips, aes(year, fill = plot_type)) + geom_histogram() + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+#year -- corrected
+ggplot(dips, aes(year, fill = plot_type)) + geom_histogram(binwidth=1)  + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+# quarterly?
+ggplot(dips, aes(year, fill = plot_type)) + geom_histogram(binwidth = 0.25)  + facet_wrap(~species) + theme_bw() + scale_fill_viridis_d(option="plasma")
+
