63fb9c48e05cdb9cf979c1b14f2059699230699c
Kaija Gahm <kaija.gahm@gmail.com>
2025-03-27 17:08:14 -0700

    example base R data wrangling with errors

---
+++ scripts/KG_more_errors.R
@@ -0,0 +1,104 @@
+# More errors
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Read in the data
+rodents <- read_csv("scripts/data/surveys_complete_77_89.csv")
+
+data(rodents)
+View(rodents)
+
+head(rodents)
+summary(rodents)
+hist(rodents$weight, col=rodents$species)
+boxplot(rodents$weight, rodents$species)
+boxplot(weight~species, data=species)
+boxplot(weight~species, data=rodents)
+
+plot(rodents$record_id, rodents$weight)
+
+table(rodents$genus)
+
+KRat <- subset(rodents, genus=="Dipodomys")
+plot(KRat$record_id, KRat$weight)
+boxplot(weight~species, data=KRat)
+
+head(KRAT)
+head(Krat)
+head(KRat)
+WeightMod <-lm(weight~species+sex+hindfoot_length, data=KRat)
+summary(WeightMod)
+
+NewDF <- data.frame(species=unique(Krat$species), CommonName = c("Merriam's", "Ord's", "Banner-Tailed"))
+NewDF <- data.frame(species=unique(KRat$species), CommonName = c("Merriam's", "Ord's", "Banner-Tailed"))
+NewDF
+unique(KRat$species)
+#we didn't notice the "sp." species in the boxplot, probably because those ones didn't have weights, either
+NewDF <- data.frame(species=unique(KRat$species), CommonName = c("Merriam's", "Ord's", "Banner-Tailed", "Unknown/Other"))
+NewDF
+#we gotta reorder!
+NewDF <- data.frame(species=sort(unique(KRat$species)), CommonName = c("Merriam's", "Ord's", "Unknown/Other", "Banner-Tailed"))
+NewDF
+
+DFplusCommonName <-left_join(x=KRats, y=NewDF, by="species")
+DFplusCommonName <-left_join(x=KRat, y=NewDF, by="species")
+dim(KRat)
+dim(DFplusCommonName)
+
+# Alternative idea: if the join fails
+dummy <- data.frame(Species=sort(unique(KRat$species)), CommonName = c("Merriam's", "Ord's", "Unknown/Other", "Banner-Tailed"))
+dummy
+
+test <-left_join(x=KRat, y=dummy, by="species") # this is helpful
+test <- left_join(x = KRat, y = dummy) # this is another error
+
+# back to our regularly scheduled programming (hah)
+
+head(DFplusCommonName)
+boxplot(weight~year, data=DFplusCommonName)
+DFplusCommonName %>% ggplot(aes(x = year, y = weight))+geom_boxplot() # this is an error in ggplot but NOT in base! interesting. We could either get into that or we could not bother pointing out the difference, just do it in ggplot from the start.
+
+DFplusCommonName %>% ggplot(aes(x = as.factor(year), y = weight))+geom_boxplot() # this is an error in ggplot but NOT in base! interesting. We could either get into that or we could not bother pointing out the difference, just do it in ggplot from the start.
+
+df <- data.frame(number = rep(c(1, 11, 2), each = 100), values = c(rnorm(100, 1, 3), rnorm(100, 3, 2), rnorm(100, 1, 1.5)))
+
+df %>%
+  ggplot(aes(x = number, y = values))+
+  geom_boxplot() # numbers are continuous
+
+
+df %>%
+  ggplot(aes(x = as.factor(as.character(number)), y = values))+
+  geom_boxplot() # factors in the wrong order--but this is too contrived to actually use as an example
+
+MinMaxMean <-function(SPECIES){
+  MIN = min(SPECIES)
+  MAX = max(SPECIES)
+  MEAN = mean(SPECIES)
+  return(MIN, MAX, MEAN)
+}
+
+MinMaxMean <-function(SPECIES){
+  MIN = min(SPECIES)
+  MAX = max(SPECIES)
+  MEAN = mean(SPECIES)
+  return(c(MIN, MAX, MEAN))
+}
+MinMaxMean(KRat$weight)
+MinMaxMean(KRat$weight, na.omit=TRUE)
+
+MinMaxMean <-function(SPECIES){
+  MIN = min(SPECIES, na.rm = T)
+  MAX = max(SPECIES, na.rm = T)
+  MEAN = mean(SPECIES, na.rm = T)
+  return(c(MIN, MAX, MEAN))
+}
+MinMaxMean(KRat$weight)
+
+SummaryStats <- c(unique(KRat$species),MinMaxMean(unique(KRat$species)))
+SummaryStats
+
+
+
+
