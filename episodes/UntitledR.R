59292bb704b7505b195b5f9322106850a5c9e87e
Kaija Gahm <kaija.gahm@gmail.com>
2025-06-09 16:54:16 -0700

    remove previous (resolved) errors so final script is shorter

--- episodes/UntitledR.R
+++ episodes/UntitledR.R
@@ -1,12 +1,9 @@
 library(tidyverse)
-surveys <- read_csv("data/surveys_complete_77_89.csv")
+surveys <- read_csv("https://raw.githubusercontent.com/carpentries-incubator/R-help-reprexes/refs/heads/main/episodes/data/surveys_complete_77_89.csv")
 glimpse(surveys)
 min(surveys$year)
 max(surveys$year)
 # Read in the data
-rodents <- read_csv("data/surveys_complete_77_89.csv")
-ggplot(x = taxa) + geom_bar()
-ggplot(aes(x = surveys$taxa)) + geom_bar()
 ggplot(surveys, aes(x = taxa)) + geom_bar()
 table(surveys$taxa)
 ?table
@@ -14,15 +11,6 @@ table(surveys$taxa, exclude = NULL)
 rodents <- surveys %>% filter(taxa == "Rodent")
 rodents_summary <- rodents %>% group_by(plot_type, month) %>% summarize(count=n())
 ggplot(rodents_summary) + geom_tile(aes(month, plot_type, fill=count))
-ggplot(rodents) + geom_tile(aes(month, plot_type), stat = "count")
-ggplot(rodents) + geom_tile(aes(month, plot_type))
-krats <- rodents %>% filter(genus == "Dipadomys") #kangaroo rat genus
-
-ggplot(krats, aes(year, fill=plot_type)) +
-  geom_histogram() +
-  facet_wrap(~species)
-krats
-print(rodents %>% count(genus))
 krats <- rodents %>% filter(genus == "Dipodomys") #kangaroo rat genus
 dim(krats)
 
@@ -35,8 +23,6 @@ rodents_subset <- rodents %>%
   filter(species == c("ordii", "spectabilis"),
          sex == c("F", "M"))
 ## Adding common names
-common_names <- data.frame(species = unique(rodents_subset$species), common_name = c("Ord's", "Banner-tailed"))
-common_names
 common_names <- data.frame(species = sort(unique(rodents_subset$species)), common_name = c("Ord's", "Banner-Tailed"))
 rodents_subset <- left_join(rodents_subset, common_names, by = "species")
 weight_model <- lm(weight ~ common_name + sex, data = rodents_subset)
